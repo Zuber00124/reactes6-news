5f4a7ea7c96d16b622b0dbf27bff97cd4b397651
Zuber <zuber00124@gmail.com>
2017-10-30 12:51:20 +0530

    Arrow Function

--- src/App.js
+++ src/App.js
@@ -1,33 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-const list = [
-  {
-    title: 'words 1',
-    url: 'http://example1.com',
-    author: 'author1',
-    num_comments: 100,
-    points: 50,
-    objectID: 1
-  },
-  {
-    title: 'words 2',
-    url: 'http://example3.com',
-    author: 'author3',
-    num_comments: 40,
-    points: 10,
-    objectID: 2
-  },
-  {
-    title: 'words 1',
-    url: 'http://example3.com',
-    author: 'author3',
-    num_comments: 10,
-    points: 5,
-    objectID: 3
-  }
-]
+import list from './list';
 
 class App extends Component {
   render() {
@@ -35,15 +9,13 @@ class App extends Component {
       <div className="App">
         <h1>
           {
-            list.map(function(item){
-             return (
-              <div>
+            list.map(item =>
+              <div key={ item.objectID }>
                <h1> <a href={ item.url}> { item.title } </a> by { item.author } </h1>
                <h4> { item.num_comments } comments | { item.points } </h4>
                
               </div>
-             ) 
-            }) 
+            ) 
           }
         </h1>
       </div>
---
+++ src/list.js
@@ -0,0 +1,28 @@
+const list = [
+    {
+      title: 'words 1',
+      url: 'http://example1.com',
+      author: 'author1',
+      num_comments: 100,
+      points: 50,
+      objectID: 1
+    },
+    {
+      title: 'words 2',
+      url: 'http://example3.com',
+      author: 'author3',
+      num_comments: 40,
+      points: 10,
+      objectID: 2
+    },
+    {
+      title: 'words 1',
+      url: 'http://example3.com',
+      author: 'author3',
+      num_comments: 10,
+      points: 5,
+      objectID: 3
+    }
+]
+
+export default list;
